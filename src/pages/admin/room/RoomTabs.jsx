0bb4376e8e210c04df8c302cb6b1f64c07a43112
thanhcanhquanca <chuanhquan89@gmail.com>
2025-05-06 15:30:26 +0700

    fix romtabs

--- src/pages/admin/room/RoomTabs.jsx
+++ src/pages/admin/room/RoomTabs.jsx
@@ -411,7 +411,7 @@ export default function RoomTabs() {
     ];
 
     return (
-        <Box sx={{ width: '100%', minWidth: '960px', fontSize: '0.7rem' }}>
+        <Box sx={{ width: '100%', fontSize: '0.7rem' }}>
             {loading && (
                 <Box sx={{ display: 'flex', justifyContent: 'center', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', zIndex: 9999 }}>
                     <CircularProgress size="2rem" />
@@ -482,6 +482,7 @@ export default function RoomTabs() {
                 open={openRoomDialog}
                 onClose={handleCloseRoomDialog}
             />
+
 
             <Grid container spacing={2.4} sx={{ minWidth: '960px' }}>
                 <Grid item xs={4}>
